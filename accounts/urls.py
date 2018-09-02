790c814fdf292b4a95c58101a5389435f046f169
arpapelia <arpapelia@gmail.com>
2018-09-02 17:09:26 +0300

    sing in ready, sign up korjattu koska objekti muotoa nimi sähköposti salasana

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_auth, name='login'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth import authenticate, login
 
 def signup(request):
     if request.method == 'POST':
-        #user has info and wants account
+        #user has info and wants account (if would be GET its just that user wants something from the site
+        #e.g. entering url to browser and pressing enter
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'signup.html', {'error':'Username already exists'})
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
-                auth.login(request, user)
+                user = User.objects.create_user(request.POST['username'], request.POST['username'] , request.POST['password1'])
+                login(request, user)
+                print (user)
                 return redirect('home')
         else:
             return render(request, 'signup.html', {'error': 'Sasswords need to match!'})
     else:
-        #user wants to enter info
+        #user wants to enter info (e.g. updating browser page)
         return render(request, 'signup.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_auth(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        print (user)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
 
 def logout(request):
     return render(request, 'signup.html')
